90c4d4c1569334f7e68ddd7fa9373e5c817f97a5
na2s78 <nassim.aliouane78@gmail.com>
2025-05-09 18:27:11 +0200

    tetris.h

---
+++ tetris.h
@@ -0,0 +1,22 @@
+#ifndef TETRIS_H
+#define TETRIS_H
+
+#define TAILLE 10
+#define TAILLE2 5
+#define MAX_SCORES 10
+
+// Structure pour stocker les scores
+// et les noms des joueurs
+typedef struct {
+    char name[50];
+    int score;
+} Score;
+
+
+void init_tab(int tab[TAILLE][TAILLE]);
+void affichage(int tab[TAILLE][TAILLE], int taille);
+int placer(int tab[TAILLE][TAILLE], int piece[TAILLE2][TAILLE2], int col);
+int vider_ligne_remplies(int tab[TAILLE][TAILLE]);
+
+
+#endif
